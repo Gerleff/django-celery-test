381de5648126aafc32103cb3ac8d7671b456989b
Gerleff <69368761+Gerleff@users.noreply.github.com>
2021-05-17 23:11:30 +0300

    Update tests.py

--- backoffice/api/tests.py
+++ backoffice/api/tests.py
@@ -6,6 +6,10 @@ from django.test.client import Client
 
 class AccountTestCase(TestCase):
     fixtures = ['test_accounts.json']
+    ACCOUNTS = {'bad_status': '867f0924-a917-4711-9399-0b179a96392b',
+                'good_status': '26c940a1-7228-4ea2-a3bc-e6460b172040',
+                'bad_substraction': '7badc8f8-65bc-449a-8cde-855234ac63e1',
+                'extra_bad_substraction': '5597cc3d-c948-48a0-b711-393edf20d9c0'}
 
     def _test_get_response(self, response_endpoint: str):
         c = Client()
@@ -30,33 +34,22 @@ class AccountTestCase(TestCase):
         self._test_get_response('/api/ping')
 
     def test_status(self):
-        bad_status = '867f0924-a917-4711-9399-0b179a96392b'
-        good_status = '7badc8f8-65bc-449a-8cde-855234ac63e1'
-
-        self._test_get_response(f'/api/status/{bad_status}')
-        self._test_get_response(f'/api/status/{good_status}')
+        self._test_get_response(f"/api/status/{ACCOUNTS['bad_status']}")
+        self._test_get_response(f"/api/status/{ACCOUNTS['good_status']}")
 
     def test_add(self):
-        bad_status = '867f0924-a917-4711-9399-0b179a96392b'
-        good_status = '7badc8f8-65bc-449a-8cde-855234ac63e1'
         patch_data = {'addition': 77777}
         error_data = {'addition': -77777}
-
-        self._test_patch_response(f'/api/add/{bad_status}', patch_data)
-        self._test_patch_response(f'/api/add/{good_status}', patch_data)
-        self._test_patch_response(f'/api/add/{good_status}', error_data)
+        self._test_patch_response(f"/api/add/{ACCOUNTS['bad_status']}", patch_data)
+        self._test_patch_response(f"/api/add/{ACCOUNTS['good_status']}", patch_data)
+        self._test_patch_response(f"/api/add/{ACCOUNTS['good_status']}", error_data)
 
     def test_substract(self):
-        bad_status = '867f0924-a917-4711-9399-0b179a96392b'
-        good_status = '26c940a1-7228-4ea2-a3bc-e6460b172040'
-        bad_substraction = '7badc8f8-65bc-449a-8cde-855234ac63e1'
-        extra_bad_substraction = '5597cc3d-c948-48a0-b711-393edf20d9c0'
         patch_data = {'substraction': 111}
         error_data = {'substraction': -111}
-
-        self._test_patch_response(f'/api/substract/{bad_status}', patch_data)
-        self._test_patch_response(f'/api/substract/{good_status}', patch_data)
-        self._test_patch_response(f'/api/substract/{bad_substraction}', patch_data)
-        self._test_patch_response(f'/api/substract/{extra_bad_substraction}', patch_data)
-        self._test_patch_response(f'/api/substract/{good_status}', error_data)
+        self._test_patch_response(f"/api/substract/{ACCOUNTS['bad_status']}", patch_data)
+        self._test_patch_response(f"/api/substract/{ACCOUNTS['good_status']}", patch_data)
+        self._test_patch_response(f"/api/substract/{ACCOUNTS['bad_substraction']}", patch_data)
+        self._test_patch_response(f"/api/substract/{ACCOUNTS['extra_bad_substraction']}", patch_data)
+        self._test_patch_response(f"/api/substract/{ACCOUNTS['good_status']}", error_data)
 
